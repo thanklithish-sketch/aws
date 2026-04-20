82fdbdf0e411cbdd9fd555f5352ce54a459c5d5c
thanklithish-sketch <thanklithish@gmail.com>
2026-04-20 16:39:13 +0530

    Rename EC2 instance tag to 'webhost-server1'

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "webins" {
   key_name                    = "secondary account"
   vpc_security_group_ids = [aws_security_group.websg.id] //security groups must be defined within []
   tags = {
-    Name = "web-server1"
+    Name = "webhost-server1"
   }
 }
 # by default vpc creates a eni and then default ebs

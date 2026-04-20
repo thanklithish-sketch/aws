9ef1f56720e48547b2b59bc40c0a5aeeed7bd4aa
thanklithish-sketch <thanklithish@gmail.com>
2026-04-20 15:41:57 +0530

    Add S3 backend configuration for Terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terra-state-bucket-us-east-1 "
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    #dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
+}

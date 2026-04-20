d613653a9ac36838bc616b03cc1e8125afccb03f
thanklithish-sketch <thanklithish@gmail.com>
2026-04-20 15:46:22 +0530

    Fix S3 bucket name formatting in s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terra-state-bucket-us-east-1 "
+    bucket         = "terra-state-bucket-us-east-1"
     key            = "infra/terraform.tfstate"
     region         = "us-east-1"
     #dynamodb_table = "terraform-locks"
